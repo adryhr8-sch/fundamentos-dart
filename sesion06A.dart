87ed9b3ce1a060e609ce07dd533902a3e94805cb
adryhr8-sch <adrian.hernandezramirez@cesunbc.edu.mx>
2026-09-24 15:54:01 -0700

    Funciones, clases, clases abstractas y promesas.

---
+++ sesion06A.dart
@@ -0,0 +1,15 @@
+//Sesion 06 A
+
+void main(){
+  print("Inicio del programa");
+  gethttpRequest("http://www.cesun.edu.mx").then((value){
+    print(value);
+  });
+  print("Fin del programa");  
+}
+
+Future gethttpRequest(String url){
+  return Future.delayed(Duration(seconds: 1), () {
+    return "Promesa asincrona finalizada";
+  });
+}
